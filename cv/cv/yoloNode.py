75be5b9175acebd12cb39ac70b8b9b6fcebe254d
Matthew Martin <matthewmartin@mines.edu>
2026-04-22 15:14:41 -0600

    adding cv package as a real package!!! :)

---
+++ cv/cv/yoloNode.py
@@ -0,0 +1,51 @@
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+from ultralytics import YOLO
+import cv2
+
+
+class YoloHumanDetectionNode(Node):
+    def __init__(self):
+        super().__init__('yolo_human_detection')
+
+        # Load model
+        self.model = YOLO("best.pt")
+
+        # Bridge between ROS and OpenCV
+        self.bridge = CvBridge()
+
+        # Subscribe to camera topic
+        self.subscription = self.create_subscription(
+            Image,
+            '/depth_cam/RGB/image_raw',   # <-- change if needed
+            self.image_callback,
+            10
+        )
+
+        self.get_logger().info("YOLO Human Detection Node Started")
+
+    def image_callback(self, msg):
+        # Convert ROS image → OpenCV image
+        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+        # Run YOLO
+        results = self.model(frame, conf=0.3)
+
+        # Draw detections
+        annotated = results[0].plot()
+
+        # Show result
+        cv2.imshow("YOLO Detection", annotated)
+        cv2.waitKey(1)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = YoloHumanDetectionNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
---
+++ cv/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'cv'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gooman84',
+    maintainer_email='matthewmartin@mines.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+             # format: 'command_name = package.module:function'
+            'yolo_node = cv.yoloNode:main',
+        ],
+    },
+)
